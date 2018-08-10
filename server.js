3d57bc11062ca04f6a71ba2e829c8ab9b4967f4e
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-08-09 20:26:25 -1000

    changed stuff in the server

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
-console.log("hello");
-console.log("conflict?");
-console.log("jyama");
-console.log('DLeaguer is me Jeff B');
+const net = require("net");
+
+const clients = [];
+
+const server = net.createServer((client) => {
+    client.write("WELCOME");
+    client.on("data", data => {
+
+        const msg = data.toString();
+        if (msg.includes("/Start")) {
+            client.write("hi")
+        } 
+             
+        clients.map(user => {
+            user.write(msg);
+        })
+    });
+
+ clients.push(client);
+});
+
+server.listen(6969, () => { 
+    console.log("Server listening on port 6969");
+});
