e1bb304420a29301e82b67ac544626670bcee475
DLeaguer <immostlikely@hotmail.com>
2018-08-10 09:08:53 -1000

    server.js & client.js write to each other

--- client.js
+++ client.js
@@ -1,6 +1,18 @@
 const net = require('net');
 
+console.log('\n\nCL process.argv =')
+console.log(process.argv)
+
+let header = "GET /apply HTTP/1.1\n\nhost: localhost:8080"
+// let slashIndex = process.argv[2].indexOf("/");
+// let host = process.argv[2].slice(0, slashIndex);
+// let path = process.argv[2].slice(slashIndex);
+
 const client = net.createConnection(8080, '0.0.0.0', () => {
+
+  client.write(header)
+  // client.write("GET /apply HTTP/1.1\nhost: localhost:8080");
+  // client.write("GET " + path + " HTTP/1.1\nhost: " + host);
   client.on('data', data => {
     console.log(data.toString());
   });
--- server.js
+++ server.js
@@ -1,83 +1,34 @@
 const net = require('net');
 
+console.log('\n\nCL process.argv')
+console.log(process.argv)
+let header = '\n\nHTTP/1.1 200 OK\n\nServer: SPARTASERVE'
+// let slashIndex = process.argv[2].indexOf("/");
+// let host = process.argv[2].slice(0, slashIndex);
+// let path = process.argv[2].slice(slashIndex);
+
 let clients = [];
 
 const server = net.createServer(client => {
-  console.log('CL client.remoteAddress =', client.remoteAddress)
-  console.log('CL client.remotePort =', client.remotePort)
-  console.log('CL client.username =', client.username)
 
-  // Identify client and push to clients list:
-  client.id = client.remoteAddress + ':' + client.remotePort;
-  client.username;
-  client.usernameHasBeenSet = false;
-  clients.push(client);
-  console.log('CL client =', client)
-  console.log('CL CONNECTED: ' + client.id);
-  console.log('CL CLIENT CONNECTED');
-  
-  client.write('\nWELCOME TO SPARTASERVE');
-  client.write('\n')
-  client.write('\nWhat is Your Name')
+  client.write(header)
+  // client.write('\n\nHTTP/1.1 200 OK\n\nServer: SpartaServe');
+  // client.write("GET " + path + " HTTP/1.1\nhost: " + host);
 
-   // Handle incoming data:
+  console.log('\nCLIENT CONNECTED\n')
+
+  client.write('\n\nWelcome to SpartaServe')
+
   client.on('data', data => {
-    console.log('CL data =', data)
-    const dataStr = data.toString().slice(0, -1);
-    console.log('CL dataStr =',dataStr)
-    if (!client.username) {
-      if (dataStr.toLowerCase().includes('admin')) {
-        client.write(`\nKeyword "ADMIN" reserved. Choose another username: `);
-      } else {
-        client.username = dataStr;
-        client.write(`\nHello ${dataStr}\n`);
-      }
-    }
-    handleIncomingData(client, dataStr);
-
     console.log(data.toString());
-
-    const msg = data.toString();
-
-    clients.forEach(socket => {
-      if(client !== socket) {
-        socket.write(msg);
-      }
-    });
+    let msg = data.toString()
   });
-
+  
   clients.push(client);
-
-  // Remove client when connection has been closed:
-  client.on('end', function () {
-    let i = clients.indexOf(client);
-    console.log('\nCL i =', i)
-    clients.splice(i,1);
-    console.log(`Client ${i} Session Ended`)
-  })
-
-   // Allow server to send "ADMIN" messages to all clients:
-   process.stdin.on('data', data => {
-    client.write(`[ADMIN] ${data.toString().slice(0, -1)}`);
-  });
-
-});
+})
+  
 
 server.listen(8080, () => {
-  console.log('Server listening on port 8080');
+  console.log('\nServer listening on port 8080');
 });
 
-function handleIncomingData(client, data) {
-  if (client.username && !client.usernameHasBeenSet) {
-    // Client's first input will be set as the client's username:
-    console.log(client.id + ' SET USERNAME: ' + client.username);
-    client.usernameHasBeenSet = true;
-  } else if (client.usernameHasBeenSet) {
-    // Client's message will be logged and dispatched to other clients:
-    clients.forEach(client => {
-      if (client === client) return;
-      client.write(`${client.username}: "${data}"`);
-    });
-    console.log('SERVER BCAST FROM ' + client.id + ' : ' + data);
-  }
-}
