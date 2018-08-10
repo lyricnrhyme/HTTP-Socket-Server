ba4b603b9f1b598c68fc1d222d9c9f609c5c6d28
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2018-08-09 21:05:56 -1000

    commit?

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 
-const client = net.createConnection(6969, "34.217.133.117", () => {
+const client = net.createConnection(2877, "34.219.80.229", () => {
     client.write("WELCOME!");
     client.on("data", data => {
         // console.log(data.toString());
